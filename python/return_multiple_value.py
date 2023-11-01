70e942c234f7a241cc72645c91400dc36abeaa93
kacamata09 <muh.ansharazhari@gmail.com>
2023-11-01 16:59:12 +0700

    try return multiple in python

---
+++ python/return_multiple_value.py
@@ -0,0 +1,12 @@
+
+
+def return_banyak():
+    return 'halo', 'you', ['suyogari'], 99, {"hitori" : "bocchi"}
+
+print(return_banyak())
+# ternyara return nya jadi tipe set
+
+salam, watashi, list1, int1, dict1 = return_banyak()
+
+print(salam)
+
