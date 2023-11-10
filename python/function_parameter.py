a0f93bdfb3cb27cb4ceaa48cad09c2e713287520
kacamata09 <muh.ansharazhari@gmail.com>
2023-11-10 10:49:52 +0700

    try function as parameter

---
+++ python/function_parameter.py
@@ -0,0 +1,22 @@
+def filterComment(comment, filterKataComment):
+    newComment = filterKataComment(comment)
+    return newComment
+
+def filterKata(kata):
+    if kata in ["sialan", "anj", 'asw', 'anjim', 'anjing']:
+        return "waduh bro bro"
+    else:
+        return kata
+
+def kataBaikChecker(kata):
+    if kata not in ["sialan", "anj", 'asw', 'anjim', 'anjing']:
+        return "baik bro"
+    else:
+        return "tidak baik bro"
+
+    
+comment = 'anjim'
+dahlah = filterComment(comment, filterKata)
+yaaa = filterComment(comment, kataBaikChecker)
+print(dahlah)
+print(yaaa)
